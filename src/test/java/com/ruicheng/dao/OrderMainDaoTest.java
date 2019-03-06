57289304e07797ed1b717061cf6efbb0b61352a5
WuRuiCheng <365232822@qq.com>
2019-03-06 10:08:47 +0800

    a little progress

---
+++ src/test/java/com/ruicheng/dao/OrderMainDaoTest.java
@@ -0,0 +1,17 @@
+package com.ruicheng.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ruicheng
+ * on 2019/3/6.
+ */
+public class OrderMainDaoTest {
+    @Test
+    public void findByBuyerOpenid() throws Exception {
+
+    }
+
+}
